98d535c4d2bd1366afd74a7f03fd34edce788b51
Arshan Attar <arshanattar17@gmail.com>
2026-09-13 16:13:18 +0530

    Remove GitHub Actions deployment workflow for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Using relative paths so GitHub Pages subfolder deployment works out of the box
+  base: '/',
 })
